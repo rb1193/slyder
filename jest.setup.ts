99fb4386cebb98142b77570e2310d3e28bb1eec2
hn_ryanb <rbrown_1193@hotmail.com>
2022-03-15 20:54:48 +1300

    Fake pointer event object to allow passing event properties for testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+
+const pointerEventProps = ['clientX', 'clientY', 'pageX', 'pageY', 'isPrimary', 'pointerType'];
+class PointerEventFake extends Event {
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    pointerEventProps.forEach((prop) => {
+      if (props[prop as keyof PointerEventInit] != null) {
+        // @ts-ignore
+        this[prop] = props[prop as keyof PointerEventInit];
+      }
+    });
+  }
+}
+
+// @ts-ignore
+global.PointerEvent = PointerEventFake
